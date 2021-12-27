6d187f4f305be1ef5501ba63a6b69076516526e2
abdollahzangian <a.zangian2462@gmail.com>
2021-12-27 14:31:10 +0330

    some change on project like change name variabels and improv GUI

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import os
 import tkinter as tk
 import tkinter.font as tkFont
 import datetime
+import webbrowser
 
 
 
-
+# the functions to kill procces of "adobe conncet" program 
 def close_program(): 
          os.system("taskkill /f /im connect.exe")
          
 
-
+# main functions to check the entry time and close "adobe conncet" program
 def check(saat,daghighe):
         print(saat)
         print(daghighe)
@@ -26,14 +27,10 @@ def check(saat,daghighe):
                  close_program()
                  break
 
-            
+def open_link(url):
+        webbrowser.open_new(url)   
 
-
-
-
-
-
-
+# design GUI 
 class App:
     def __init__(self, root):
         #setting title
@@ -41,31 +38,34 @@ class App:
         #setting window size
         width=443
         height=181
+        
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        GLabel_2=tk.Label(root)
+
+        #setting "ساعت " label
+        lbl_hour=tk.Label(root)
         ft = tkFont.Font(family='TAHOMA',size=13)
-        GLabel_2["font"] = ft
-        GLabel_2["fg"] = "#333333"
-        GLabel_2["justify"] = "center"
-        GLabel_2["text"] = "ساعت "
-        GLabel_2.place(x=90,y=60,width=89,height=45)
+        lbl_hour["font"] = ft
+        lbl_hour["fg"] = "#333333"
+        lbl_hour["justify"] = "center"
+        lbl_hour["text"] = "ساعت"
+        lbl_hour.place(x=90,y=60,width=89,height=45)
 
-
+        #setting "ساعت " entry
         self.saat=tk.StringVar()
-        GLineEdit_895= tk.Entry(root,textvariable=self.saat)
-        GLineEdit_895["borderwidth"] = "1px"
+        entry_hour= tk.Entry(root,textvariable=self.saat)
+        entry_hour["borderwidth"] = "1px"
         ft = tkFont.Font(family='TAHOMA',size=10)
-        GLineEdit_895["font"] = ft
-        GLineEdit_895["fg"] = "#333333"
-        GLineEdit_895["justify"] = "center"
-        GLineEdit_895.place(x=160,y=70,width=50,height= 20)
+        entry_hour["font"] = ft
+        entry_hour["fg"] = "#333333"
+        entry_hour["justify"] = "center"
+        entry_hour.place(x=160,y=70,width=50,height= 20)
         
-
+        #setting "دقیقه " label
         lbl_minute=tk.Label(root)
         ft = tkFont.Font(family='TAHOMA',size=13)
         lbl_minute["font"] = ft
@@ -74,76 +74,82 @@ class App:
         lbl_minute["text"] = "دقیقه"
         lbl_minute.place(x=220,y=70,width=50,height=25)
 
+        #setting "دقیقه " entry
         self.daghigh=tk.StringVar()
-        GLineEdit_656=tk.Entry(root,textvariable=self.daghigh)
-        GLineEdit_656["borderwidth"] = "1px"
+        entry_minute=tk.Entry(root,textvariable=self.daghigh)
+        entry_minute["borderwidth"] = "1px"
         ft = tkFont.Font(family='TAHOMA',size=10)
-        GLineEdit_656["font"] = ft
-        GLineEdit_656["fg"] = "#333333"
-        GLineEdit_656["justify"] = "center"
-        GLineEdit_656.place(x=270,y=70,width=50,height=20)
+        entry_minute["font"] = ft
+        entry_minute["fg"] = "#333333"
+        entry_minute["justify"] = "center"
+        entry_minute.place(x=270,y=70,width=50,height=20)
         
-        GButton_953=tk.Button(root)
-        GButton_953["activebackground"] = "#126e9c"
-        GButton_953["bg"] = "#1e90ff"
+        #setting "فعال کردن " button
+        btn_active=tk.Button(root)
+        btn_active["activebackground"] = "#126e9c"
+        btn_active["bg"] = "#1e90ff"
         ft = tkFont.Font(family='TAHOMA',size=8)
-        GButton_953["font"] = ft
-        GButton_953["fg"] = "#000000"
-        GButton_953["justify"] = "center"
-        GButton_953["text"] = "فعال کردن"
-        GButton_953.place(x=170,y=120,width=60,height=20)
-        GButton_953["command"] = self.GButton_953_command
+        btn_active["font"] = ft
+        btn_active["fg"] = "#000000"
+        btn_active["justify"] = "center"
+        btn_active["text"] = "فعال کردن"
+        btn_active.place(x=170,y=120,width=60,height=20)
+        btn_active["command"] = self.btn_active_command
        
-
-        GButton_424=tk.Button(root)
-        GButton_424["activebackground"] = "#ea2617"
-        GButton_424["bg"] = "#e62a2a"
+        #setting "خروج " button
+        btn_exit=tk.Button(root)
+        btn_exit["activebackground"] = "#ea2617"
+        btn_exit["bg"] = "#e62a2a"
         ft = tkFont.Font(family='TAHOMA',size=10)
-        GButton_424["font"] = ft
-        GButton_424["fg"] = "#000000"
-        GButton_424["justify"] = "center"
-        GButton_424["text"] = "خروج"
-        GButton_424.place(x=240,y=120,width=40,height=20)
-        GButton_424["command"] = self.GButton_424_command
+        btn_exit["font"] = ft
+        btn_exit["fg"] = "#000000"
+        btn_exit["justify"] = "center"
+        btn_exit["text"] = "خروج"
+        btn_exit.place(x=240,y=120,width=40,height=20)
+        btn_exit["command"] = self.btn_exit_command
 
-        GLabel_826=tk.Label(root)
+        #setting "راحت برو بخواب " label
+        lbl_header=tk.Label(root)
         ft = tkFont.Font(family='TAHOMA',size=15)
-        GLabel_826["font"] = ft
-        GLabel_826["fg"] = "#333333"
-        GLabel_826["justify"] = "center"
-        GLabel_826["text"] = " (: راحت برو بخواب "
-        GLabel_826.place(x=70,y=10,width=240,height=42)
+        lbl_header["font"] = ft
+        lbl_header["fg"] = "#333333"
+        lbl_header["justify"] = "center"
+        lbl_header["text"] = " (: راحت برو بخواب "
+        lbl_header.place(x=70,y=10,width=240,height=42)
 
-        GLabel_425=tk.Label(root)
+        #setting " A.zangian طراحی شده توسط " label
+        lbl_made_by=tk.Label(root)
         ft = tkFont.Font(family='TAHOMA',size=10)
-        GLabel_425["font"] = ft
-        GLabel_425["fg"] = "#333333"
-        GLabel_425["justify"] = "center"
-        GLabel_425["text"] = " A.zangian طراحی شده توسط"
-        GLabel_425.place(x=20,y=140,width=190,height=30)
+        lbl_made_by["font"] = ft
+        lbl_made_by.bind("<Button-1>", lambda e: open_link("https://github.com/abdollahzangian/Rahat_Bekhab"))
+        lbl_made_by["fg"] = "blue"
+        lbl_made_by["justify"] = "center"
+        lbl_made_by["text"] = " A.zangian طراحی شده توسط"
+        lbl_made_by.place(x=20,y=140,width=190,height=30)
+        lbl_made_by.config(cursor="heart")
+        # chenge style when hover on lable " A.zangian طراحی شده توسط"
+        def Enter_style(e):
+            lbl_made_by["fg"]="red"
+        def Leave_style(e):
+            lbl_made_by["fg"]="blue"
+
+
+        lbl_made_by.bind("<Enter>", Enter_style)
+        lbl_made_by.bind("<Leave>", Leave_style)
+
         
-    def chars(self):
-        print("command")
-    def number(self):
-        print("command")
+        
 
 
-    def chars(self):
-        print("command")
-    def number(self):
-        print("command")
-
-
-    def GButton_953_command(self):
+    def btn_active_command(self):
         check(self.saat.get(),self.daghigh.get())
         
-        
-       
 
-
-
-    def GButton_424_command(self):
+    def btn_exit_command(self):
         root.destroy()
+
+    
+
 
 if __name__ == "__main__":
     root = tk.Tk()
