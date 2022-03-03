28b0b142c704e9128de97c12d95f16c03513a406
abdollahzangian <a.zangian2462@gmail.com>
2022-03-03 05:41:36 +0330

    add robot to type a message depend on comments of student

--- robot.py
+++ robot.py
@@ -1,3 +1,5 @@
+import model
+from time import sleep
 from pyautogui import click, press, typewrite
 import pytesseract 
 import cv2
@@ -12,21 +14,48 @@ def type_message(message):
      click(1097,693)
      typewrite(message)
      press("enter")
+     
+
+
 
 def extractText():
 
-        pytesseract.pytesseract.tesseract_cmd = r"C:\Python39\Lib\site-packages\pytesseract"
+     
+        # point : 993, 577, 1365, 730
+        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
         while True:
-        
-            process_area = ImageGrab.grab(bbox=(993, 577, 1365, 730))
+            process_area = ImageGrab.grab(include_layered_windows=False,bbox=(725, 577, 1365, 730))
             text = pytesseract.image_to_string(
                     cv2.cvtColor(np.array(process_area), cv2.COLOR_BGR2GRAY),
                     lang = "eng"
                 )
-            print(text)
+
+            
+            if "khaste nabashid" in text:
+                type_message()
+                sleep(1)
+                model.Core.close_program()
+                break
+
+
+
+                
+                 
+
+           
+    
+
+                
+
+                    
+
+
+                                                                           
+
             
 
 
+extractText()
 
 
 
